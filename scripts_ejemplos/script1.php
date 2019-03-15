950b9e496f7f3475b00e97eb16be722aa43a337c
santiago <santiagodchk@gmail.com>
2019-03-15 12:01:13 -0500

    nuevo php

---
+++ scripts_ejemplos/script1.php
@@ -0,0 +1,16 @@
+<center> Programacion avanzada </center>
+<?php
+  $nombre = "Peter";
+  $apellido = "Watson";
+  $edad = 30;
+  $salario = 800;
+
+
+    echo "<center>HOLI</center>";
+    echo "Nombre: ".$nombre ."<br>";
+    echo "Apellido: ".$apellido ."<br>" ;
+    echo "Edad: ".$edad ;
+    echo "<br>Salario: ".$salario;
+
+ ?>
+<center>Fin del programa</center>
---
+++ scripts_ejemplos/script2.php
@@ -0,0 +1,18 @@
+<center>Calculadora basica</center>
+<?php
+
+  $num1 = 10;
+  $num2 = 5 ;
+
+  $suma = $num1+$num2;
+  $resta = $num1-$num2;
+  $mult = $num1*$num2;
+  $div = $num1/$num2;
+
+  echo "<center>";
+  echo "<br>La suma es" .$suma;
+  echo "<br>La resta es" .$resta;
+  echo "<br>La multiplicacion es" .$mult;
+  echo "<br>La division es" .$div;
+
+ ?>
