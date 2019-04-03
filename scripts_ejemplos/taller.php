ce6557148c9549e8fcfe606bec92a5e4586f850b
santiago <santiagodchk@gmail.com>
2019-04-03 04:00:26 -0500

    taller

--- scripts_ejemplos/taller.php
+++ scripts_ejemplos/taller.php
@@ -1,3 +1,28 @@
+<html>
+<head>
+  <title>Taller</title>
+  <link rel= "icon" type= "image/ico" href="icons/calculator.png"/>
+</head>
+<body >
+<center><b><font face="Verdana" size="15" color="BLACK">Desarrollo Taller</font></b></center><br>
+<table border="0" align="center">
+  <form name="frm1" action= "calculadora.php" method= "post">
+    <tr>
+              <td><b><font face="Arial" color="BLACK">Total de numeros generados:</font></b><br>
+              <input type="text" name= "num1" required><br><br></td>
+            </tr>
+            <tr>
+                      <td><b><font face="Arial" color="BLACK">Total de numeros generados:</font></b><br>
+                      <input type="text" name= "num1" required><br><br></td>
+                    </tr>
+
+                        </form>
+</table>
+
+</body>
+</html>
+
+
 <?php
 
 echo "<b>1er punto</b></br></br>";
@@ -40,13 +65,50 @@ echo "</tr></table>";
 echo "3er punto</b></br></br>";
 echo "<table border=1><tr>";
 for ($i=1; $i<=100 ; $i=$i+1) {
-  echo "<td>". $i. "</td></br>";
+  echo "<td>". $i. "</td>";
 }
 echo "</tr></table>";
 // --------------------------------------------
+
 echo "4to punto</br></br>";
+echo "<table border=1><tr>";
 for ($i=2; $i<=100 ; $i=$i+2) {
-  echo $i. "</br>";
+  echo "<td>". $i. "</td>";
+}
+echo "</tr></table>";
+
+echo "</br>5to punto";
+echo "<table border=1><tr>";
+for ($i=1; $i<=10 ; $i=$i+1) {
+  echo "<td>". rand(1,10)."</td>";
+}
+echo "</tr></table>";
+echo "6to punto</br></br>";
+$suma=0; //incializamos la suma como cero
+for($i=1;$i<=100;$i++){
+$suma +=$i; //por cada ciclo sumamos el valor de $i a lo que llevamos en $suma
+ //imprimimos sus valores
+echo "la suma es:". $suma. "</br>" ;
 }
 
+echo "7to punto</br></br>";
+echo "<table border=1><tr>";
+$x = 3;
+for ($i=0; $i<=100 ; $i=$i+1) {
+  if($i%$x==0){
+    echo "<td>". $i. "</td>";
+  }
+
+}
+echo "</tr></table>";
+
+echo "</br>8to punto";
+$n1 = $_POST['num1'];
+$n2 = $_POST['num2'];
+$op = $_POST['operar'];
+echo "<table border=1><tr>";
+for ($i=1; $i<=10 ; $i=$i+1) {
+  echo "<td>". rand(1,100)."</td>";
+}
+echo "</tr></table>";
  ?>
